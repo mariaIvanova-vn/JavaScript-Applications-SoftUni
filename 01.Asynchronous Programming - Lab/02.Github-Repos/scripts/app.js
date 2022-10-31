d9e36fab6b2aaffc5586f9b17311210c7c97d7bc
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-31 11:15:31 +0200

    add

--- 01.Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
+++ 01.Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,35 @@
 function loadRepos() {
-	console.log("TODO...");
+	let userName = document.getElementById('username').value;
+
+	fetch(`https://api.github.com/users/${userName}/repos`)
+		.then(handleResponse)
+		.then(handleData)
+		.catch(handleError);
+}
+
+function handleResponse(response) {
+	if (response.ok == false) {
+		throw new Error(`Error: ${response.status} ${response.statusText}`);
+	}
+	return response.json();
+}
+
+function handleData(data) {
+	let list = document.getElementById('repos');
+
+	let items = data.map(repo => {
+		let li = document.createElement('li');
+		let a = document.createElement('a');
+		a.href = repo.html_url;
+		a.textContent = repo.full_name;
+		li.appendChild(a);
+
+		return li;
+	})
+	list.replaceChildren(...items);
+}
+
+function handleError(error) {
+	let list = document.getElementById('repos');
+	list.textContent=error.message;
 }
