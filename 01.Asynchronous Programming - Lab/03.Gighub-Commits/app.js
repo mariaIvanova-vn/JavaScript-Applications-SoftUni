06ee87616835399681efbbd6cf67211096287456
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-31 13:33:48 +0200

    add

--- 01.Asynchronous Programming - Lab/03.Gighub-Commits/app.js
+++ 01.Asynchronous Programming - Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,20 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+    let commits = document.getElementById('commits');
+    try {
+        const responce = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
+
+        if (responce.ok == false) {
+            throw new Error(`Error: ${responce.status} (Not Found)`);
+        }
+        let data = await responce.json();
+        for (const com of data) {
+            let item = document.createElement('li');
+            item.textContent = `${com.commit.author.name}: ${com.commit.message}`;
+            commits.appendChild(item);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
 }
