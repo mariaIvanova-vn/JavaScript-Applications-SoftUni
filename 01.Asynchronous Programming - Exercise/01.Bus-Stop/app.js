ffddbf2c62b5f02e91974d06878b629dfddcdaec
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-31 15:41:21 +0200

    add 01.Buses

--- 01.Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ 01.Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
 function getInfo() {
-    console.log("TODO...");
+    let baseURL = 'http://localhost:3030/jsonstore/bus/businfo';
+    let inputEl = document.getElementById('stopId').value;
+    let ulElements = document.getElementById('buses');
+    let divEl = document.getElementById('stopName');
+
+    fetch(`${baseURL}/${inputEl}`)
+        .then(response => response.json())
+        .then(data => {
+            let buses=data.buses;
+            let name=data.name;
+
+            divEl.textContent=name;
+            ulElements.innerHTML='';
+            Object.keys(buses).forEach(bus=>{
+                let liElement=document.createElement('li');
+                liElement.textContent=`Bus ${bus} arrives in ${buses[bus]} minutes`
+                ulElements.appendChild(liElement);
+            })
+        })
+        .catch(error=>{
+            divEl.textContent="Error";
+            ulElements.innerHTML='';
+        })
 }
