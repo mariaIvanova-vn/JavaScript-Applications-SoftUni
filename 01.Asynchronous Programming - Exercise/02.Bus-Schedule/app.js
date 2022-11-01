19b1ee7238cdeb53707b500cef93da957d9c9839
mariaIvanova-vn <maria_vn86@abv.bg>
2022-11-01 15:07:21 +0200

    add

--- 01.Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ 01.Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
 
+    let info = document.querySelector('.info');
+    let departButton = document.getElementById('depart');
+    let arriveButton = document.getElementById('arrive');
+
+    let busStops = {
+        next: 'depot'
+    };
+
     function depart() {
-        console.log('Depart TODO...');
+        departButton.disabled = true;
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStops.next}`)
+            .then(response => response.json())
+            .then(data => {
+                busStops = JSON.parse(JSON.stringify(data));
+                info.textContent = `Next stop ${busStops.name}`
+            })
+            .catch(error=>console.log(error));
+            arriveButton.disabled=false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent=`Arriving at ${busStops.name}`;
+        departButton.disabled = false;
+        arriveButton.disabled=true;
     }
 
     return {
