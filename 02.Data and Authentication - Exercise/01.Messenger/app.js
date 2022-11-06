e212ceec1e6993bf965f621ee8659522aadf76f7
mariaIvanova-vn <maria_vn86@abv.bg>
2022-11-06 10:00:20 +0200

    add

--- 02.Data and Authentication - Exercise/01.Messenger/app.js
+++ 02.Data and Authentication - Exercise/01.Messenger/app.js
@@ -1,5 +1,33 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit').addEventListener('click', addComment)
+    document.getElementById('refresh').addEventListener('click', displayComment);
+}
+
+let url = `http://localhost:3030/jsonstore/messenger`;
+let displayArea = document.getElementById('messages');
+
+async function addComment() {
+    let author = document.querySelector('[name="author"]').value;
+        let content = document.querySelector('[name="content"]').value;
+
+        await fetch(url, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({author, content})
+        })
+        console.log(author, content);
+}
+
+async function displayComment() {
+    let response = await fetch(url);
+        let data = await response.json();
+        let output = [];
+        
+        for (const message of Object.values(data)) {
+            output.push(`${message.author}: ${message.content}`);
+        }
+        displayArea.value = output.join('\n');
+        console.log(data);
 }
 
 attachEvents();
