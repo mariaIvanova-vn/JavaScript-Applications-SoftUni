17637e4f408c52b9d8768b4005ca67042aa1fe97
mariaIvanova-vn <maria_vn86@abv.bg>
2022-11-07 11:07:45 +0200

    add solution for 03.Students

--- 02.Data and Authentication - Exercise/03.Students/app.js
+++ 02.Data and Authentication - Exercise/03.Students/app.js
@@ -1,1 +1,46 @@
-console.log('TODO...');
+async function solve() {
+    const form = document.getElementById('form');
+    const table = document.querySelector('tbody');
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+
+    form.addEventListener('submit', onSubmit);
+
+    table.innerHTML = '';
+    let response = await fetch(url);
+    let result = await response.json();
+
+    let allStudents = Object.values(result);
+
+    allStudents.forEach(x => {
+        let row = document.createElement('tr');
+
+        for (let data in x) {
+            if (data == '_id') {
+                continue;
+            }
+            let cell = document.createElement('td');
+            cell.textContent = x[data];
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    });
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        let data = new FormData(form)
+        let student = Object.fromEntries(data.entries());
+
+        if (Object.values(student).includes('')) {
+            return;
+        }
+
+        await fetch(url, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(student)
+        });
+    }
+}
+
+solve()
