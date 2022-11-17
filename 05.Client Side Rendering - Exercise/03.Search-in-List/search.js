9ef8b3f48b34426cd5bd1cbcf34296372e50f1b9
mariaIvanova-vn <maria_vn86@abv.bg>
2022-11-17 14:38:19 +0200

    03.Search-in-List

--- 05.Client Side Rendering - Exercise/03.Search-in-List/search.js
+++ 05.Client Side Rendering - Exercise/03.Search-in-List/search.js
@@ -1,6 +1,35 @@
 import {html, render} from "../node_modules/lit-html/lit-html.js";
 import {towns} from "./towns.js";
 
+let template = html `
+<ul>
+${towns.map(town=>{
+  return html `<li id=${town}>${town}</li>`
+})}
+</ul>
+`;
+
+let root = document.getElementById('towns');
+render(template, root);
+
+document.querySelector('button').addEventListener('click', search);
+
 function search() {
+   let text = document.getElementById('searchText').value;
+   let resultHtml=document.getElementById('result');
+   let count=0;
+   let result = towns.filter(t=>{
+      if(t.includes(text)){
+         let match=document.getElementById(`${t}`);
+         match.setAttribute('class', 'active');
+         count++;
+         return t;
+      }else{
+         let match=document.getElementById(`${t}`);
+         match.setAttribute('class', 'none');
+         return t;
+      }
+   });
    
+   resultHtml.textContent=`${count} matches found`;
 }
