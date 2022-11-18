bdc4066f91ecefe16ee274b0effa940fc181988d
mariaIvanova-vn <maria_vn86@abv.bg>
2022-11-18 11:51:45 +0200

    add 04.Fill dropdown

--- 05.Client Side Rendering - Exercise/04.Fill-Dropdown/dropdown.js
+++ 05.Client Side Rendering - Exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,31 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+
+async function getAllItems() {
+    const response = await fetch('http://localhost:3030/jsonstore/advanced/dropdown');
+    let data = await response.json();
+    return data;
 }
+
+let items = Object.values(await getAllItems());
+
+let template = html`${items.map((x) => html`<option value=${x._id}>${x.text}</option>`)}`;
+let main = document.getElementById('menu');
+render(template, main);
+
+document.querySelector('input[type="submit"]').addEventListener('click', addItem);
+
+async function addItem(e) {
+    e.preventDefault();
+    let text = document.querySelector('#itemText').value;
+    let response = await fetch('http://localhost:3030/jsonstore/advanced/dropdown', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ text })
+    })
+    let data = await response.json();
+    items.push(data);
+    if (response.ok) {
+        let template = html`${items.map((x) => html`<option value=${x._id}>${x.text}</option>`)}`;
+        render(template,main)
+    }
+} 
